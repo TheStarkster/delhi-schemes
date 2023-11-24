8c98cda53f012602def62193cbe8b941c05be6d0
Gurkaran Singh <gurkaran@grevity.in>
2023-11-24 11:57:26 +0530

    Update find_scheme_modal.jsx

--- src/components/find_scheme_modal.jsx
+++ src/components/find_scheme_modal.jsx
@@ -1,33 +1,111 @@
-const Modal = ({ isOpen, onClose, children }) => {
-  return isOpen && <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+import React, { useState } from 'react';
 
-  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-          <div className="sm:flex sm:items-start">
-            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-              <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-              </svg>
-            </div>
-            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-              <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+const Modal = ({ isOpen, onClose }) => {
+  const [age, setAge] = useState(0);
+
+  const handleDateChange = (e) => {
+    const dob = new Date(e.target.value);
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const m = today.getMonth() - dob.getMonth();
+  
+    if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+      age--;
+    }
+  
+    setAge(age)
+  };
+  
+
+  return isOpen && (
+    <div className="relative" style={{ zIndex: 999 }} aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:w-full sm:max-w-lg">
+            
+            {/* Form starts here */}
+            <div className="bg-white px-8 py-5 sm:p-6">
+              <h3 className="text-lg leading-6 font-medium text-gray-900">Find the Schemes for You!</h3>
+              <div className="mt-5">
+                <form>
+                  <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700">Gender <span className="text-red-500">*</span></label>
+                    <select className="mt-1 pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                      <option>Male</option>
+                      <option>Female</option>
+                      <option>Others</option>
+                    </select>
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700">Date of Birth <span className="text-red-500">*</span></label>
+                    <input type="date" className="mt-1 pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleDateChange} />
+                    {age > 0 && <p className="text-sm pt-2 text-gray-500" style={{ fontStyle: "italic" }}>You are {age} years old.</p>}
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700">Are you a resident of Delhi? <span className="text-red-500">*</span></label>
+                    <div className="mt-2">
+                      <label className="inline-flex items-center">
+                        <input type="radio" name="resident" className="form-radio" value="yes" />
+                        <span className="ml-2">Yes</span>
+                      </label>
+                      <label className="inline-flex items-center ml-6">
+                        <input type="radio" name="resident" className="form-radio" value="no" />
+                        <span className="ml-2">No</span>
+                      </label>
+                    </div>
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700">What category do you belong to?</label>
+                    <select className="mt-1 pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                      <option>General</option>
+                      <option>OBC</option>
+                      <option>SC</option>
+                      <option>ST</option>
+                      <option>Minority</option>
+                    </select>
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700">Are you differently abled?</label>
+                    <div className="mt-2">
+                      <label className="inline-flex items-center">
+                        <input type="radio" name="differentlyAbled" className="form-radio" value="yes" />
+                        <span className="ml-2">Yes</span>
+                      </label>
+                      <label className="inline-flex items-center ml-6">
+                        <input type="radio" name="differentlyAbled" className="form-radio" value="no" />
+                        <span className="ml-2">No</span>
+                      </label>
+                    </div>
+                  </div>
+
+                  <div className="mb-6">
+                    <label className="block text-sm font-medium text-gray-700">What is your annual income?</label>
+                    <div className="mt-1 relative rounded-md shadow-sm">
+                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <span className="text-gray-500 sm:text-sm">₹</span>
+                      </div>
+                      <input type="number" className="pl-7 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00" />
+                    </div>
+                  </div>
+                </form>
               </div>
+            </div>
+            {/* Form ends here */}
+
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button type="submit" className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto">Search</button>
+              <button type="button" onClick={onClose} className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
             </div>
           </div>
         </div>
-        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
-          <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
-        </div>
       </div>
     </div>
-  </div>
-</div>
+  );
 };
 
 export default Modal;
