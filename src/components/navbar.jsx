057e30a1feeab990649b89a8bad5fcdb161eede8
Gurkaran Singh <gurkaran@grevity.in>
2023-11-29 23:08:17 +0530

    contract coding

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [top, setTop] = useState(true);
   const [language, setLanguage] = useState("English"); // Default language
   const [isSearchPage, setIsSearchPage] = useState(false); // State to track if it's the search page
-  const [showDropdown, setShowDropdown] = useState(false);
+  // const [showDropdown, setShowDropdown] = useState(false);
   const [isOpen, setIsOpen] = useState(false); // For hamburger menu
   const router = useRouter(); // Use the useLocation hook
 
@@ -32,77 +32,130 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', scrollHandler);
   }, []);
 
-  return <header className={`bg-white sticky left-0 top-0 right-0 z-20 ${!top && `bg-white shadow-lg`}`}>
-    <div
-      className="mx-auto justify-between flex h-28 max-w-screen-xl items-center gap-2 px-4 sm:px-6 lg:px-8"
-    >
-      <a className="flex items-center text-blue-500" href="/">
-        <span className="sr-only">Home</span>
-        <img src="/logo.jpeg" alt="logo" className="object-contain mr-4 w-full max-w-[90px]" />
-        <div className="ml-2">
-          <div className="text-2xl font-bold" style={{ letterSpacing: '2px' }}>DCPCR</div>
-          <div className="text-xs italic hidden md:block">
-            Delhi Commission for
-            <br />
-            Protection of Child Rights
+
+  const [fontSize, setFontSize] = useState('normal'); // normal, large, x-large
+  const [contrast, setContrast] = useState('normal'); // normal, high
+
+  // Handlers for font size and contrast
+  const handleFontSizeChange = (size) => {
+    setFontSize(size);
+    // Apply the font size change to the body or a specific container
+    document.body.className = `font-${size}`;
+  };
+
+  const handleContrastChange = (contrastLevel) => {
+    setContrast(contrastLevel);
+    // Apply the contrast change to the body or a specific container
+    document.body.className = `contrast-${contrastLevel}`;
+  };
+
+  return <>
+    <div className="flex justify-end items-center space-x-2 p-2 bg-white border-b max-w-7xl mx-auto">
+      {/* Font Size Controls */}
+      <button
+        onClick={() => handleFontSizeChange('normal')}
+        className="px-2 py-1 text-sm font-medium text-gray-700 bg-gray-200 rounded hover:bg-gray-300"
+      >
+        A
+      </button>
+      <button
+        onClick={() => handleFontSizeChange('large')}
+        className="px-2 py-1 text-base font-medium text-gray-700 bg-gray-200 rounded hover:bg-gray-300"
+      >
+        A
+      </button>
+      <button
+        onClick={() => handleFontSizeChange('x-large')}
+        className="px-2 py-1 text-lg font-medium text-gray-700 bg-gray-200 rounded hover:bg-gray-300"
+      >
+        A
+      </button>
+
+      {/* Contrast Controls */}
+      <button
+        onClick={() => handleContrastChange('normal')}
+        className="px-2 py-1 text-sm font-medium text-gray-700 bg-gray-200 rounded hover:bg-gray-300"
+      >
+        Normal Contrast
+      </button>
+      <button
+        onClick={() => handleContrastChange('high')}
+        className="px-2 py-1 text-sm font-medium text-gray-700 bg-gray-200 rounded hover:bg-gray-300"
+      >
+        High Contrast
+      </button>
+    </div>
+    <header className={`bg-white sticky left-0 top-0 right-0 z-20 ${!top && `bg-white shadow-lg`}`}>
+      <div
+        className="mx-auto justify-between flex h-28 max-w-screen-xl items-center gap-2 px-4 sm:px-6 lg:px-8"
+      >
+        <a className="flex items-center text-blue-500" href="/">
+          <span className="sr-only">Home</span>
+          <img src="/logo.jpeg" alt="logo" className="object-contain mr-4 w-full max-w-[90px]" />
+          <div className="ml-2">
+            <div className="text-2xl font-bold" style={{ letterSpacing: '2px' }}>DCPCR</div>
+            <div className="text-xs italic hidden md:block">
+              Delhi Commission for
+              <br />
+              Protection of Child Rights
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
 
-      <div className="flex items-center">
-      <nav className={`${isOpen ? 'block' : 'hidden'} absolute bg-white top-full left-0 w-full lg:static lg:w-auto lg:block`}>
-      <ul className="flex flex-col lg:flex-row lg:items-center gap-6 lg:gap-4 p-4 lg:p-0">
-            {usePathname() === '/' && (
-              <li className="hidden md:block" onClick={() => router.push('/search')}>
-                <div className="flex items-center border-2 border-gray-300 rounded-md overflow-hidden">
-                  <SearchIcon className="text-gray-500 ml-4" />
-                  <input
-                    type="text"
-                    placeholder="Search..."
-                    className="py-2 px-4 focus:outline-none"
-                    readOnly
-                  />
-                </div>
-              </li>
-            )}
+        <div className="flex items-center">
+          <nav className={`${isOpen ? 'block' : 'hidden'} absolute bg-white top-full left-0 w-full lg:static lg:w-auto lg:block`}>
+            <ul className="flex flex-col lg:flex-row lg:items-center gap-6 lg:gap-4 p-4 lg:p-0">
+              {usePathname() === '/' && (
+                <li className="hidden md:block" onClick={() => router.push('/search')}>
+                  <div className="flex items-center border-2 border-gray-300 rounded-md overflow-hidden">
+                    <SearchIcon className="text-gray-500 ml-4" />
+                    <input
+                      type="text"
+                      placeholder="Search..."
+                      className="py-2 px-4 focus:outline-none"
+                      readOnly
+                    />
+                  </div>
+                </li>
+              )}
 
-            {
-              usePathname() !== '/' && <>
-                {
-                  usePathname() !== '/search' && (<li>
-                    <a className="flex items-center border-2 border-orange-500 p-2 rounded-lg rounded-md px-3 py-2 text-sm font-medium" href="/search">
-                    <SearchIcon className="h-5 w-5 text-iceland_poppy-500 mr-2" /> <p className="mr-1 text-iceland_poppy-500">Search</p>
+              {
+                usePathname() !== '/' && <>
+                  {
+                    usePathname() !== '/search' && (<li>
+                      <a className="flex items-center border-2 border-orange-500 p-2 rounded-lg rounded-md px-3 py-2 text-sm font-medium" href="/search">
+                        <SearchIcon className="h-5 w-5 text-iceland_poppy-500 mr-2" /> <p className="mr-1 text-iceland_poppy-500">Search</p>
+                      </a>
+                    </li>)
+                  }
+                  <li>
+                    <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/">
+                      Home
                     </a>
-                  </li>)
-                }
-                <li>
-                  <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/">
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/resources">
-                    Resources
-                  </a>
-                </li>
-                <li>
-                  <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/compensation">
-                    Compensation
-                  </a>
-                </li>
-                <li>
-                  <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/essential-docs">
-                    Essential Docs
-                  </a>
-                </li>
-                <li>
-                  <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/">
-                    Disability Information
-                  </a>
-                </li>
-              </>
-            }
-            {/* <div className="relative">
+                  </li>
+                  <li>
+                    <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/resources">
+                      Resources
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/compensation">
+                      Compensation
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/essential-docs">
+                      Essential Docs
+                    </a>
+                  </li>
+                  <li>
+                    <a className="text-gray-500 transition hover:bg-iceland_poppy-500 hover:text-white rounded-md px-3 py-2 text-sm font-medium" href="/">
+                      Disability Information
+                    </a>
+                  </li>
+                </>
+              }
+              {/* <div className="relative">
               <button style={{ color: "#e4750a" }} onClick={() => setShowDropdown(!showDropdown)} className="flex items-center gap-2 text-gray-500 hover:text-blue-500">
                 <IconLanguage className="h-5 w-5" />
                 <span>{language}</span>
@@ -114,31 +167,32 @@ const Navbar = () => {
                 </ul>
               </div>)}
             </div> */}
-          </ul>
-        </nav>
-        <button
-          onClick={() => setIsOpen(!isOpen)}
-          className="rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 lg:hidden"
+            </ul>
+          </nav>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 lg:hidden"
           >
-          <span className="sr-only">Toggle menu</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
+            <span className="sr-only">Toggle menu</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </>
 }
 
 export default Navbar
